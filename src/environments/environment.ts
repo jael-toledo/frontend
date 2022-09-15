2565911ee38d8ff354f3cca095566c9a4ec9a833
jael-toledo <jael.toledo@sansano.usm.cl>
2022-09-15 05:00:58 -0300

    production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_URL: 'https://enigmatic-lowlands-10443.herokuapp.com'
 };
 
